e770f3783559f4e646c12cd1ef372425b97632ce
YMH <1256815659@qq.com>
2020-03-20 16:06:12 +0800

    update

--- server/page/readwrite/readwrite.go
+++ server/page/readwrite/readwrite.go
@@ -6,20 +6,22 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+//Read is to read message
 func Read(c *websocket.Conn) {
 	mt, message, err := c.ReadMessage()
 	if err != nil {
 		log.Println("read:", err)
-		break
+		return
 	}
 	log.Printf("recv: %s", message)
-	log.Printf("recv type: %t", mt)
+	log.Printf("recv type: %d", mt)
 }
 
+//Write is to write message
 func Write(c *websocket.Conn, mt int, message []byte) {
-	err = c.WriteMessage(mt, message)
+	err := c.WriteMessage(mt, message)
 	if err != nil {
 		log.Println("write:", err)
-		break
+		return
 	}
 }
